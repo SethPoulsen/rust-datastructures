48b0f1eae515fc88e361f4477644e905fad7a75e
Seth Poulsen <setpous@amazon.com>
2019-05-24 13:54:03 -0700

    Added factorial function

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+    // integration test go here
 }
 
 pub mod stack;
+pub mod recursion;
---
+++ src/recursion.rs
@@ -0,0 +1,35 @@
+
+pub fn fac_iterative(n: u32) -> u32 {
+    let mut acc = 1;
+    for i in 1..(n + 1) {
+        acc = acc * i;
+    }
+    acc
+}
+
+pub fn fac_recursive(n: u32) -> u32 {
+    match n {
+        0 => 1,
+        n => n * fac_recursive(n - 1)
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::fac_iterative;
+    use super::fac_recursive;
+
+    #[test]
+    fn basics() {
+        assert_eq!(fac_iterative(0), 1);
+        assert_eq!(fac_iterative(1), 1);
+        assert_eq!(fac_iterative(5), 120);
+        assert_eq!(fac_iterative(10), 3628800);
+
+        assert_eq!(fac_recursive(0), 1);
+        assert_eq!(fac_recursive(1), 1);
+        assert_eq!(fac_recursive(5), 120);
+        assert_eq!(fac_recursive(10), 3628800);
+    }
+}
