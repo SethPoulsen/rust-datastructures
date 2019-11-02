2af11faf51a3b11ceca9017471ff0fe3238aab9d
Seth Poulsen <poulsenseth@yahoo.com>
2019-11-02 15:47:54 -0500

    List is finally almost as elegant as I want it to be.

--- src/list.rs
+++ src/list.rs
@@ -1,47 +1,41 @@
-use std::mem;
 
-// #[derive(Copy, Clone)] // more advanced, maybe for starters just copy it?
 pub enum List<T> {
-    Nothing, 
+    Nil, 
     Cons(Box<T>, Box<List<T>>),
 }
 
 impl <T> List<T> {
 
     pub fn new() -> Self {
-        List::Nothing
+        List::Nil
     }
 
-    // pub fn push(&mut self, data: T) -> List<T> {
-    //     List::Cons(Box::new(data), Box::new(match &self))
-    // }
+    pub fn push(self, data: T) -> List<T> {
+        List::Cons(Box::new(data), Box::new(self))
+    }
 
-    // pub fn peek(&self) -> Option<&T> {
-    //     match &self {
-    //         List::Nothing => None,
-    //         List::Cons(data, next) => Some(&data)
-    //     }
-    // }
+    pub fn peek(&self) -> Option<&T> {
+        match &self {
+            List::Nil => None,
+            List::Cons(data, _) => Some(&data)
+        }
+    }
+
+    pub fn pop(self) -> (List<T>, Option<T>) {
+        match self {
+            List::Nil => (List::Nil, None),
+            List::Cons(data, next) => (*next, Some(*data))
+        }
+    }
 
     pub fn size(&self) -> i32 {
         match &self {
-            List::Nothing => 0,
-            List::Cons(data, next) => 1 + next.size()
+            List::Nil => 0,
+            List::Cons(_, next) => 1 + next.size()
         }
     }
 
 }
-
-// impl <T> Drop for List<T> {
-//     fn drop(&mut self) {
-//         let mut cur_link = mem::replace(&mut self.head, None);
-
-//         while let Some(mut boxed_node) = cur_link {
-//             cur_link = match mem::replace(&mut boxed_node.next, None);
-//         }
-//     }
-// }
-
 
 #[cfg(test)]
 mod test {
@@ -50,35 +44,30 @@ mod test {
     #[test]
     fn basics() {
         let mut list = List::<i32>::new();
-
-        // assert_eq!(None, match &list {
-        //     Nothing => None,
-        //     List::Cons(data, next) => Some(data)
-        // });
-        // assert_eq!(list.peek(), None);
+        assert_eq!(list.peek(), None);
         assert_eq!(list.size(), 0);
 
-        // list = List::Cons(1, Box::new(list));
-        // list = List::Cons(2, Box::new(list));
-        // list = List::Cons(3, Box::new(list));
+        list = list.push(1);
+        list = list.push(2);
+        list = list.push(3);
+        assert_eq!(list.size(), 3);
 
-        // assert_eq!(list.size(), 3);
+        assert_eq!(list.peek(), Some(&3));
 
-        // assert_eq!(list.peek(), Some(&3));
-        // assert_eq!(list.pop(), Some(3));
-        // assert_eq!(list.pop(), Some(2));
-        // assert_eq!(list.size(), 1);
+        let mut temp = list.pop(); assert_eq!(temp.1, Some(3));
+        temp = temp.0.pop(); assert_eq!(temp.1, Some(2));
 
-        // list.push(4);
-        // list.push(5);
-        // assert_eq!(list.size(), 3);
+        list = temp.0.push(4);
+        list = list.push(5);
+        assert_eq!(list.size(), 3);
 
-        // assert_eq!(list.peek(), Some(&5));
-        // assert_eq!(list.pop(), Some(5));
-        // assert_eq!(list.pop(), Some(4));
-        // assert_eq!(list.pop(), Some(1));
-        // assert_eq!(list.pop(), None);
-        // assert_eq!(list.size(), 0);
+        assert_eq!(list.peek(), Some(&5));
+        temp = list.pop(); assert_eq!(temp.1, Some(5));
+        temp = temp.0.pop(); assert_eq!(temp.1, Some(4));
+        temp = temp.0.pop(); assert_eq!(temp.1, Some(1));
+        temp = temp.0.pop(); assert_eq!(temp.1, None);
+
+        assert_eq!(temp.0.size(), 0);
 
     }
 }
